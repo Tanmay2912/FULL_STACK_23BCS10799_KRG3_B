10b58b558f46ceff8febf74d5027ff9d2ae7b089
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-11-17 10:46:33 +0530

    Fix formatting in Header component

--- Practical EST/Question 1/Header.jsx
+++ Practical EST/Question 1/Header.jsx
@@ -3,9 +3,9 @@ export default function Header({ username }) {
     <header>
       <h2>Header Section</h2>
 
-      {/* Passing username further to NavBar */}
       <NavBar username={username} />
     </header>
   );
 }
+
 import NavBar from "./NavBar";
